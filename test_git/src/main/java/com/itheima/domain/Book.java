f76fb2e4e04fab7b3b82538a8de66f793a42734e
cxh <123@qq.com>
2018-11-08 18:00:03 +0800

    添加了两行代码

--- test_git/src/main/java/com/itheima/domain/Book.java
+++ test_git/src/main/java/com/itheima/domain/Book.java
@@ -4,8 +4,10 @@ import java.util.Date;
 
 public class Book {
     private Date productDate;
+	private String name;
 
     public static void main(String[] args) {
         System.out.println("是一本好书");
+		System.out.println("那是什么书呢");
     }
 }
