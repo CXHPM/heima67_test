9918e6025bb5dadb4251e6ebf5377cf56d42d60e
cxh <123@qq.com>
2018-11-08 17:47:41 +0800

    添加了几行代码

--- test_git/src/main/java/com/itheima/domain/Book.java
+++ test_git/src/main/java/com/itheima/domain/Book.java
@@ -1,4 +1,11 @@
 package com.itheima.domain;
 
+import java.util.Date;
+
 public class Book {
+    private Date productDate;
+
+    public static void main(String[] args) {
+        System.out.println("是一本好书");
+    }
 }
