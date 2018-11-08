63d61aa92f369578608092de3a1401c48546fc06
cxh <123@qq.com>
2018-11-08 19:00:20 +0800

    添加了两行新的代码

--- test_git/src/main/java/com/itheima/domain/Book.java
+++ test_git/src/main/java/com/itheima/domain/Book.java
@@ -4,8 +4,10 @@ import java.util.Date;
 
 public class Book {
     private Date productDate;
+    private Integer price;
 
     public static void main(String[] args) {
         System.out.println("是一本好书");
+        System.out.println("卖的有点贵啊");
     }
 }
