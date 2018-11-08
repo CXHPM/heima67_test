c08d0fa9f9a910810a43bbba143228d2871c9cc7
cxh <123@qq.com>
2018-11-08 17:19:46 +0800

    添加了一行代码

--- test_git/src/main/java/com/itheima/domain/Book.java
+++ test_git/src/main/java/com/itheima/domain/Book.java
@@ -1,8 +1,9 @@
 package com.itheima.domain;
 
 public class Book {
-    public static void main(String[] args) {
+    private String name;
 
+    public static void main(String[] args) {
         System.out.println("this is a book");
     }
 }
